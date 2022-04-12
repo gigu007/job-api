8e6228e623890572728b91f9867bd1e9e2514fd7
gigu007 <gideonukaegbu007@gmail.com>
2022-04-12 11:39:42 +0100

    another commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,14 @@
 const User= require('../models/User')
 const {StatusCodes}=require('http-status-codes');
 const {BadRequestError}= require('../errors')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async(req,res)=>{
-   
-    // const {name,email,password}= req.body
-    // const salt = await bcrypt.genSalt(10)
-    // const hashPassword = await bcrypt.hash(password,salt)
-    // const tempUser = {name,email,password:hashPassword}
-    // if(!name || !email || !password){
-    //     throw new BadRequestError('Please provide name email and password')
-    // }
-
-    //mongoose validator
-    const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({user})
+   const user = await User.create({...req.body})
+   const token = jwt.sign({userID:user._id,name:user.name}, 'jwtSecret',{
+       expiresIN:'30d',
+   })
+    res.status(StatusCodes.CREATED).json({user:user.name},token)
 }
 const login =async(req,res)=>{
     res.send('login user')
