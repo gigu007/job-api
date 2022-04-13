6d52dcd8d07856fb272ce1f04c4215a86ed9470e
gigu007 <gideonukaegbu007@gmail.com>
2022-04-13 11:29:44 +0100

    another commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,33 @@
-const User= require('../models/User')
-const {StatusCodes}=require('http-status-codes');
-const {BadRequestError}= require('../errors')
-const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
-const register = async(req,res)=>{
-   const user = await User.create({...req.body})
-   const token = jwt.sign({userID:user._id,name:user.name}, 'jwtSecret',{
-       expiresIN:'30d',
-   })
-    res.status(StatusCodes.CREATED).json({user:user.name},token)
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
-const login =async(req,res)=>{
-    res.send('login user')
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  // compare password
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
-module.exports  = {
-    register,
-    login,
+
+module.exports = {
+  register,
+  login,
 }
